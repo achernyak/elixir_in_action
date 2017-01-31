74b4fa725da1d21ce473d024b251d96a22575af2
achernyak <artemchernyak@gmail.com>
2017-01-30 20:56:35 -0600

    longest lines

--- enum_streams.ex
+++ enum_streams.ex
@@ -8,5 +8,23 @@ defmodule EnumStreams do
     filtered_lines!(path)
     |> Enum.map(&String.length/1)
   end
-  
+
+  def longest_line_length!(path) do
+    filtered_lines!(path)
+    |> Stream.map(&String.length/1)
+    |> Enum.reduce(0, &max/2)
+  end
+
+  def longest_line!(path) do
+    filtered_lines!(path)
+    |> Enum.reduce("", &longer_line/2)
+  end
+
+  defp longer_line(line1, line2) do
+    if String.length(line1) > String.length(line2) do
+      line1
+    else
+      line2
+    end
+  end
 end
