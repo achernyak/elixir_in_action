5d2aecf9bd37535a36b750f5634c3256d7ffcad0
achernyak <artemchernyak@gmail.com>
2017-02-02 20:36:05 -0600

    basic server

--- todo/todo_server.ex
+++ todo/todo_server.ex
@@ -1,5 +1,16 @@
 defmodule TodoServer do
-  
+  def start do
+    spawn(fn -> loop(TodoList.new) end)
+  end
+
+  defp loop(tood_list) do
+    new_todo_list = receive do
+      message ->
+	process_message(todo_list, message)
+    end
+
+    loop(new_todo_list)
+  end
 end
 
 defmodule TodoList do
