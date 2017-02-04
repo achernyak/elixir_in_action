ac02d03be38503891f9fae0f9bc05c451562417f
achernyak <artemchernyak@gmail.com>
2017-02-04 09:48:15 -0600

    gen server todo server

--- todo/todo_server.ex
+++ todo/todo_server.ex
@@ -1,7 +1,84 @@
+defmodule ServerProcess do
+  def start(callback_module) do
+    spawn(fn ->
+      initial_state = callback_module.init
+      loop(callback_module, initial_state)
+    end)
+  end
+
+  def call(server_pid, request) do
+    send(server_pid, {:call, request, self})
+
+    receive do
+      {:response, response} ->
+	response
+    end
+  end
+
+  def cast(server_pid, request) do
+    send(server_pid, {:cast, request})
+  end
+
+  defp loop(callback_module, current_state) do
+    receive do
+      {:call, request, caller} ->
+        {response, new_state} = callback_module.handle_call(
+          request,
+          current_state
+        )
+        send(caller, {:response, response})
+
+        loop(callback_module, new_state)
+      {:cast, request} ->
+	new_state = callback_module.handle_cast(
+	request,
+	current_state
+      )
+	loop(callback_module, new_state)
+    end
+  end
+end
+
 defmodule TodoServer do
   def start do
-    server = spawn(fn -> loop(TodoList.new) end)
-    
+    ServerProcess.start(TodoServer)
+  end
+
+  def init do
+    TodoList.new
+  end
+
+  def add_entry(todo_server, new_entry) do
+    ServerProcess.cast(todo_server, {:add_entry, new_entry})
+  end
+
+  def entries(todo_server, date) do
+    ServerProcess.call(todo_server, {:entries, self, date})
+  end
+
+  def update_entry(todo_server, new_entry) do
+    ServerProcess.cast(todo_server, {:update_entry, new_entry})
+  end
+
+  def delete_entry(todo_server, entry_id) do
+    SErverProcess.cast(todo_server, {:delete_entry, entry_id})
+  end
+
+  def handle_cast({:add_entry, new_entry}, todo_list) do
+    TodoList.add_entry(todo_list, new_entry)
+  end
+
+  def handle_cast({:update_entry, new_entry}, todo_list) do
+    TodoList.update_entry(todo_list, new_entry)
+  end
+
+  def handle_cast({:delete_entry, entry_id}, todo_list) do
+    TodoList.delete_entry(todo_list, entry_id)
+  end
+
+  def handle_call({:entries, date}, todo_list) do
+    {TodoList.entries(todo_list, date), todo_list}
+  end
 end
 
 defmodule TodoList do
