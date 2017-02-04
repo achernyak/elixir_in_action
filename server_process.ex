2149588650659b7a16e472ead02d4c57f994e28b
achernyak <artemchernyak@gmail.com>
2017-02-04 09:28:54 -0600

    key value store

--- server_process.ex
+++ server_process.ex
@@ -28,3 +28,17 @@ defmodule ServerProcess do
     end
   end
 end
+
+defmodule KeyValueStore do
+  def init do
+    HashDict.new
+  end
+
+  def handle_call({:put, key, value}, state) do
+    {:ok, HashDict.put(state, key, value)}
+  end
+
+  def handle_call({:get, key}, state) do
+    {HashDict.get(state, key), state}
+  end
+end
