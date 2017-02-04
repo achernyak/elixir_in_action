87728fcefb92811e6bdd6d72b482fbc5e1e2c1ce
achernyak <artemchernyak@gmail.com>
2017-02-04 09:38:30 -0600

    async HashDict

--- server_process.ex
+++ server_process.ex
@@ -48,10 +48,18 @@ defmodule KeyValueStore do
     HashDict.new
   end
 
-  def handle_call({:put, key, value}, state) do
-    {:ok, HashDict.put(state, key, value)}
+  def get(pid, key) do
+    ServerProcess.call(pid, {:get, key})
   end
 
+  def put(pid, key, value) do
+    ServerProcess.cast(pid, {:put, key, value})
+  end
+
+  def handle_cast({:put, key, value}, state) do
+    HashDict.put(state, key, value)
+  end
+  
   def handle_call({:get, key}, state) do
     {HashDict.get(state, key), state}
   end
