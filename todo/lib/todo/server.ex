73acd9eae05ef5c6fae8ca871ef7e275b6ba8978
achernyak <artemchernyak@gmail.com>
2017-02-05 18:50:05 -0600

    named server

--- todo/lib/todo/server.ex
+++ todo/lib/todo/server.ex
@@ -1,8 +1,8 @@
 defmodule Todo.Server do
   use GenServer
   
-  def start do
-    GenServer.start(Todo.Server, nil)
+  def start(name) do
+    GenServer.start(Todo.Server, name)
   end
   def add_entry(todo_server, new_entry) do
     GenServer.cast(todo_server, {:add_entry, new_entry})
@@ -20,30 +20,33 @@ defmodule Todo.Server do
     GenServer.cast(todo_server, {:delete_entry, entry_id})
   end
 
-  def init(_) do
-    {:ok, Todo.List.new}
+  def init(name) do
+    {:ok, { name, Todo.Database.get(name) || Todo.List.new}}
   end
 
-  def handle_cast({:add_entry, new_entry}, todo_list) do
+  def handle_cast({:add_entry, new_entry}, {name, todo_list}) do
     new_state = Todo.List.add_entry(todo_list, new_entry)
-    {:noreply, new_state}
+    {:noreply, {name,  new_state}}
   end
 
-  def handle_cast({:update_entry, new_entry}, todo_list) do
+  def handle_cast({:update_entry, new_entry}, {name, todo_list}) do
     new_state = Todo.List.update_entry(todo_list, new_entry)
-    {:noreply, new_state}
+    {:noreply, {name, new_state}}
   end
 
-  def handle_cast({:delete_entry, entry_id}, todo_list) do
+  def handle_cast({:delete_entry, entry_id}, {name, todo_list}) do
     new_state = Todo.List.delete_entry(todo_list, entry_id)
-    {:noreply, new_state}
+    {:noreply, {name, new_state}}
   end
 
-  def handle_call({:entries, date}, _, todo_list) do
+  def handle_call({:entries, date}, _, {name, todo_list}) do
     {
       :reply,
       Todo.List.entries(todo_list, date),
-      todo_list 
+      {name, todo_list} 
     }
   end
+
+  def handle_info(:stop, state), do: {:stop, :normal, state}
+  def handle_info(_, state), do {:noreply, state}
 end
