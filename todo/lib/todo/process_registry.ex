de91bb388def308dc9c43bb13706a48aaf66bd3c
achernyak <artemchernyak@gmail.com>
2017-02-08 08:13:54 -0600

    process registry

--- todo/lib/todo/process_registry.ex
+++ todo/lib/todo/process_registry.ex
@@ -1,5 +1,32 @@
 defmodule Todo.ProcessRegistry do
   use GenServer
+  import Kernel, except: [send: 2]
+
+  def start_link do
+    IO.puts "Starting process registry"
+    GenServer.start_link(__MODULE__, nil, name: :process_registry)
+  end
+
+  def register_name(key, pid) do
+    GenServer.call(:process_registry, {:register_name, key, pid})
+  end
+
+  def whereis_name(key) do
+    GenServer.call(:process_registry, {:whereis_name, key})
+  end
+
+  def unregister_name(key) do
+    GenServer.call(:rocess_registry, {:unregister_name, key})
+  end
+
+  def send(key, message) do
+    case whereis_name(key) do
+      :undefined -> {:badarg, {key, message}}
+      pid ->
+	Kernel.send(pid, message)
+	pid
+    end
+  end
 
   def init(_) do
     {:ok, HashDict.new}
@@ -23,9 +50,15 @@ defmodule Todo.ProcessRegistry do
     }
   end
 
+  def handle_call({:unregister_name, key}, _, process_registry) do
+    {:reply, key, HashDict.delete(process_registry, key)}
+  end
+
   def handle_info({:DOWN, _, :process, pid, _}, process_registry) do
     {:noreply, deregister_pid(process_registry, pid)}
   end
+
+  def handle_info(_, state), do: {:noreply, state}
 
   defp deregister_pid(process_registry, pid) do
     Enum.reduce(
