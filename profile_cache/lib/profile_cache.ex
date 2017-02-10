d541377b21aaa19cf0eaeaa59ef78dc74ee7ae93
achernyak <artemchernyak@gmail.com>
2017-02-10 07:44:10 -0600

    moved process registry to ets

--- profile_cache/lib/profile_cache.ex
+++ profile_cache/lib/profile_cache.ex
@@ -2,23 +2,38 @@ defmodule ProfileCache do
   use GenServer
 
   def start_link do
-    GenServer.start_link(__MODULE__, [], name: :page_cache)
+    GenServer.start_link(__MODULE__, [], name: :ets_profile_cache)
   end
 
   def cached(key, fun) do
-    GenServer.call(:page_cache, {:cached, key, fun})
+    read_cached(key) ||
+      GenServer.call(:ets_profile_cache, {:cached, key, fun})
   end
 
-  def init(_) do
-    {:ok, HashDict.new}
-  end
-
-  def handle_call({:cached, key, fun}, _, cache) do
-    case HashDict.get(cache, key) do
-      nil ->
-	response = fun.()
-	{:reply, response, HashDict.put(cache, key, response)}
-      response -> {:reply, response, cache}
+  defp read_cached(key) do
+    case :ets.lookup(:ets_profile_cache, key) do
+      [{^key, cached}] -> cached
+      _ -> nil
     end
+  end
+
+  
+  def init(_) do
+    :ets.new(:ets_profile_cache, [:set, :named_table, :protected])
+    {:ok, nil}
+  end
+
+  def handle_call({:cached, key, fun}, _, state) do
+    {
+      :reply,
+      read_cached(key) || cache_response(key, fun),
+      state
+    }
+  end
+
+  defp cache_response(key, fun) do
+    response = fun.()
+    :ets.insert(:ets_profile_cache, {key, response})
+    response
   end
 end
